c9c4f114105e85176bf69f188aa7029c4e2342cc
sabirrealestate9 <sabirrealestate9@gmail.com>
2026-01-17 03:02:28 +0400

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,9 +1,8 @@
 // app-config.js
 window.APP_CONFIG = {
-  // Paste your deployed Google Apps Script Web App URL here:
-  // Example: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
   API_URL: "https://script.google.com/macros/s/AKfycbwH7ICA4_BlTvfN5FU68YgSIDe6xA1ziuZx3Ajc1KXeMZY5P4inQ_IIyBMAev7C5oh_/exec",
+  SHEET_URL: "https://docs.google.com/spreadsheets/d/1ryQPlHzOeL-IfwPUIwb0u5oQSyczVon4GEEE5zCrEtk/edit?usp=drive_link",
 
-  // Optional: your Google Sheet URL for "Open Sheet" button
-  SHEET_URL: "https://docs.google.com/spreadsheets/d/1ryQPlHzOeL-IfwPUIwb0u5oQSyczVon4GEEE5zCrEtk/edit?usp=drive_link"
+  // Optional, only if your Apps Script uses a key:
+  API_KEY: "" // leave blank if not used
 };
