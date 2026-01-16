d18e8a8b20b15962717316f5a46297aa56e16515
sabirrealestate9 <sabirrealestate9@gmail.com>
2026-01-17 02:57:21 +0400

    Create app-config.js

---
+++ app-config.js
@@ -0,0 +1,9 @@
+// app-config.js
+window.APP_CONFIG = {
+  // Paste your deployed Google Apps Script Web App URL here:
+  // Example: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
+  API_URL: "https://script.google.com/macros/s/AKfycbwH7ICA4_BlTvfN5FU68YgSIDe6xA1ziuZx3Ajc1KXeMZY5P4inQ_IIyBMAev7C5oh_/exec",
+
+  // Optional: your Google Sheet URL for "Open Sheet" button
+  SHEET_URL: "https://docs.google.com/spreadsheets/d/1ryQPlHzOeL-IfwPUIwb0u5oQSyczVon4GEEE5zCrEtk/edit?usp=drive_link"
+};
